7bf6b3a5db793a2159477e8a4295ed7d2052efbc
paekung <68011088@kmitl.ac.th>
2026-01-21 14:29:52 +0700

    p02

--- src/practicals/p02/grading.ts
+++ src/practicals/p02/grading.ts
@@ -1,1 +1,24 @@
 // Write your code below
+const args = process.argv
+
+if(args[2]){
+    const score = Number(args[2])
+
+    if(score<0){
+        console.log("Invalid input")
+    }else if (score<=49){
+        console.log("Grade is F")
+    }else if (score<=59){
+        console.log("Grade is D")
+    }else if (score<=69){
+        console.log("Grade is C")
+    }else if (score<=79){
+        console.log("Grade is B")
+    }else if (score<=100){
+        console.log("Grade is A")
+    }else{
+        console.log("Invalid input")
+    }
+} else {
+    console.log("Invalid input")
+}
