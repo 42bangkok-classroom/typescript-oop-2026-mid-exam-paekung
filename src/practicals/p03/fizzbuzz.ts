a89cc6ebd67c70eb73ea28fff635664f2f594a05
paekung <68011088@kmitl.ac.th>
2026-01-21 15:24:09 +0700

    p03

--- src/practicals/p03/fizzbuzz.ts
+++ src/practicals/p03/fizzbuzz.ts
@@ -1,20 +1,17 @@
 // Write your code below
 const argsp03 = process.argv
 
-if(!argsp03){
-    process.exit()
-}
-
-const inputp03 = Number(argsp03[2])
-
-for(let i = 1;i<=inputp03; i++){
-    if(i%3 == 0 && i%5 == 0){
-        console.log("FizzBuzz")
-    } else if (i%3 == 0){
-        console.log("Fizz")
-    } else if (i%5 == 0){
-        console.log("Buzz")
-    } else {
-        console.log(i)
+if(argsp03){
+    const inputp03 = Number(argsp03[2])
+    for(let i = 1;i<=inputp03; i++){
+        if(i%3 == 0 && i%5 == 0){
+            console.log("FizzBuzz")
+        } else if (i%3 == 0){
+            console.log("Fizz")
+        } else if (i%5 == 0){
+            console.log("Buzz")
+        } else {
+            console.log(i)
+        }
     }
 }
