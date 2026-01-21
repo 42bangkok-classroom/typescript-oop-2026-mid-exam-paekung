3d6920c016bcc50df501d620f9fcaeba92eb7932
paekung <68011088@kmitl.ac.th>
2026-01-21 15:24:22 +0700

    test

--- src/practicals/p04/number-pattern.ts
+++ src/practicals/p04/number-pattern.ts
@@ -1,1 +1,37 @@
 // Write your code below
+const argsp04 = process.argv
+
+if(!argsp04){
+    process.exit()
+}
+
+const inputp04 = Number(argsp04[2])
+
+//     1
+//    21
+//   321
+//  4321
+// 54321
+
+
+if(inputp04 <= 0){
+    process.exit()
+}
+
+let pattern = ""
+
+for(let i = 1; i<= inputp04; i++){ // แถว
+    for(let j = inputp04-i; j>0; j--){
+        pattern += "x"
+    }
+    // for(let k = 1; k<=i; k++){
+    //     pattern += k
+    // }
+    for (let num = 1; num <= inputp04; num++) {
+        pattern += num
+    }
+
+    pattern += "\n"
+}
+
+console.log(pattern)
--- src/practicals/p05/shorten-comment.ts
+++ src/practicals/p05/shorten-comment.ts
@@ -1,5 +1,18 @@
 const comment = "TypeScript is a strongly typed programming language"
 
 function shortenComment(comment: string): string {
-  // Write your code below
+  const stringArr = comment.split(" ")
+
+  const temp = stringArr.forEach((element,index) => {
+    if(element.length >= 5 && element.length <= 10){
+      stringArr.splice(index,1)
+    }
+  })
+
+  console.log(temp)
+
+  return '1'
+
 }
+
+shortenComment("Typescript is a Strongly type programming language")
