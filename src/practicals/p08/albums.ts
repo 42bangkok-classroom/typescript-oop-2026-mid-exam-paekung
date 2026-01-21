20a608552a92a656d2d7ae728d743f9be5f9373c
paekung <68011088@kmitl.ac.th>
2026-01-21 17:00:24 +0700

    last

--- src/practicals/p08/albums.ts
+++ src/practicals/p08/albums.ts
@@ -1,20 +1,45 @@
+import axios from 'axios';
+
 export interface FullAlbum {
     userId: number
     id: number
     title: string
-    photos?: {
-        albumId: number
-        id: number
-        title: string
-        url: string
-        thumbnailUrl: string
-    }[]
+    photos?: Photos[]
   }
   
+
+  interface Photos {
+    albumId: number
+    id: number
+    title: string
+    url: string
+    thumbnailUrl: string
+  }
 
 const PHOTOS_URL = 'https://jsonplaceholder.typicode.com/photos';
 const ALBUMS_URL = 'https://jsonplaceholder.typicode.com/albums';
 
-function mapPhotoToAlbum(userIds?: number[]): Promise<FullAlbum[]> {
-    
+async function mapPhotoToAlbum(userIds?: number[]): Promise<FullAlbum[]> {
+    if(!userIds){
+        return []
+    }
+
+    const responsPhotos[] = await axios.get<Photos[]>(ALBUMS_URL);
+    const responsAlbums[] = await axios.get<FullAlbum[]>(PHOTOS_URL);
+    const photos = responsPhotos.data;
+    const albums = responsAlbums.data;
+
+
+    albums.map(album => {
+        const photoDetails = album.find(album => album.id === purchaseItem.itemId)
+        const photoss = photos.map(photo => {
+            return {
+                ...photo
+            }
+        })
+
+    })
+
+
+
 }
